b4052b5afcf65817d3f49fe5247793a464c10670
CRIS7HIAN30-Femsa <cristhian.valadez@digitalfemsa.com>
2023-08-08 21:45:14 -0600

    Tabs Refactor Moves

--- src/screens/MovimientosScreen.tsx
+++ src/screens/MovimientosScreen.tsx
@@ -1,26 +1,24 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, FlatList, View} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 //Tabs
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 //Context
-import {useAppContext} from '../contexts/AppContext';
+import { useAppContext } from '../contexts/AppContext';
 
 //Types
-import {Movement} from '../types/Movements';
+import { Movement } from '../types/Movements';
 
 //Functions
-import {getMonthName} from '../utils/monthName';
-import {filterMovementsByMonth} from '../utils/timeUtils';
+import { filterMovementsByMonth } from '../utils/timeUtils';
 
 //Components
-import MonthTitle from '../customComponents/MonthTitle';
-import MovementItem from '../customComponents/MovimientoComponent';
+import { TabContent } from '../customComponents/TabContent';
 
 const MovimientosScreen = () => {
-  const {state} = useAppContext();
-  const movementsByMonths: {month: number; movements: Movement[]}[] = [];
+  const { state } = useAppContext();
+  const movementsByMonths: { month: number; movements: Movement[] }[] = [];
 
   //Organize movements
   for (let month = 0; month < 12; month++) {
@@ -28,41 +26,45 @@ const MovimientosScreen = () => {
       state.userGiftHistory,
       month,
     );
-    movementsByMonths.push({month, movements: filteredMovements});
+    movementsByMonths.push({ month, movements: filteredMovements });
   }
 
   //Filtering movements by month
   const filteredMovementsByMonths = movementsByMonths
-    .filter(({movements}) => movements.length > 0)
+    .filter(({ movements }) => movements.length > 0)
     .sort((a, b) => b.month - a.month);
 
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const routes = [
-      { key: 'all', title: 'Todos' },
-      { key: 'earned', title: 'Ganados' },
-      { key: 'spent', title: 'Usados' },
-    ];
+  const routes = [
+    { key: 'all', title: 'Todos' },
+    { key: 'earned', title: 'Ganados' },
+    { key: 'spent', title: 'Usados' },
+  ];
 
-    const renderScene = SceneMap({
-      all: () => <TabContent data={filteredMovementsByMonths} />,
-      earned: () => (
-        <TabContent
-          data={filteredMovementsByMonths.map(item => ({
-            month: item.month,
-            movements: item.movements.filter(movement => movement.operation === 'earned'),
-          }))}
-        />
-      ),
-      spent: () => (
-        <TabContent
-          data={filteredMovementsByMonths.map(item => ({
-            month: item.month,
-            movements: item.movements.filter(movement => movement.operation === 'spent'),
-          }))}
-        />
-      ),
-    });
+  const renderScene = SceneMap({
+    all: () => <TabContent data={filteredMovementsByMonths} />,
+    earned: () => (
+      <TabContent
+        data={filteredMovementsByMonths.map(item => ({
+          month: item.month,
+          movements: item.movements.filter(
+            movement => movement.operation === 'earned',
+          ),
+        }))}
+      />
+    ),
+    spent: () => (
+      <TabContent
+        data={filteredMovementsByMonths.map(item => ({
+          month: item.month,
+          movements: item.movements.filter(
+            movement => movement.operation === 'spent',
+          ),
+        }))}
+      />
+    ),
+  });
 
   return (
     <SafeAreaView style={styles.container}>
@@ -83,37 +85,12 @@ const MovimientosScreen = () => {
   );
 };
 
-const TabContent = ({ data }: any) => (
-  <FlatList
-    style={styles.flatList}
-    showsVerticalScrollIndicator={false}
-    data={data}
-    keyExtractor={item => item.month.toString()}
-    renderItem={({ item }) => (
-      <View>
-        <MonthTitle title={getMonthName(item.month)} />
-        {item.movements.map((movement: Movement) => (
-          <MovementItem
-            key={movement.id}
-            entity={movement.entity}
-            date={movement.date}
-            points={movement.points}
-            operation={movement.operation}
-          />
-        ))}
-      </View>
-    )}
-  />
-);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
   },
-  flatList: {
-    marginHorizontal: 15,
-  },
+
   itemContainer: {
     paddingVertical: 16,
     borderBottomWidth: 1,
@@ -133,7 +110,6 @@ const styles = StyleSheet.create({
   },
   tabIndicator: {
     backgroundColor: '#1723D3',
-
   },
   tabBar: {
     backgroundColor: 'white',
@@ -142,7 +118,7 @@ const styles = StyleSheet.create({
     color: 'black',
     textTransform: 'capitalize',
     fontSize: 14,
-    fontWeight: '600'
+    fontWeight: '600',
   },
 });
 
